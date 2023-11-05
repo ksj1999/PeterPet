9492c44cc681d2be13d6d1b31792df5662f05e94
ella00100 <yoon8720@gmail.com>
2023-11-05 20:59:52 +0900

    feat : login func

---
+++ app/routes/login.js
@@ -0,0 +1,39 @@
+import express from "express";
+import { selectSql } from "../database/sql";
+
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.render('login');
+});
+
+router.post('/', async (req, res) => {
+    const vars = req.body;
+    
+    console.log(req.body);
+    console.log(req.session);
+    const users = await selectSql.getUser();
+
+    users.map((user) => {
+        console.log('User ID :', user.UserId);
+        console.log('User Password: ', user.Password);
+        console.log(' ');
+        if (vars.id == user.UserId && vars.password == user.Password) {
+            console.log('login success!');
+            req.session.user = { id: user.UserId, role : 'user', checkLogin: true };
+        }
+    });
+
+
+    if (req.session.user == undefined) {
+        console.log('login failed!');
+        res.send(`<script>
+                    alert('login failed!');
+                    location.href='/';
+                </script>`)
+    } else if (req.session.user.checkLogin ) {
+        res.redirect('/home');
+    } 
+});
+
+module.exports = router;
--- app/src/index.js
+++ app/src/index.js
@@ -1,22 +1,32 @@
 import express from 'express';
 import logger from 'morgan';
 import path from 'path';
+import expressSession from "express-session";
 
+import loginRouter from '../routes/login';
 import homeRouter from '../routes/home';
 
 const PORT = 5000;
 const app = express();
 
-app.use(express.static(path.join(__dirname, './src')));
-app.use(express.urlencoded({extended: false}));
-app.use(express.json());
+app.use(express.static(path.join(__dirname, '/src')));
+app.use(express.urlencoded({ extended: false })) 
+app.use(express.json()); 
+app.use(
+    expressSession({
+        secret: "my key",
+        resave: true,
+        saveUninitialized: true,
+    })
+);
 
 app.set('views', path.join(__dirname, '../views'));
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 
-app.use('/', homeRouter);
+app.use('/', loginRouter);
+app.use('/home', homeRouter);
 
 app.listen(PORT, ()=> {
     console.log(`Server is running at http://localhost:${PORT}`)
