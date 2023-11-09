cb5d665351ddc288116be76c91f556a3fb8b53af
ella00100 <ella00100@github.com>
2023-11-10 07:39:05 +0900

    modify : Dog table

--- app/database/PeterPet_DB_SCHEMA.sql
+++ app/database/PeterPet_DB_SCHEMA.sql
@@ -13,6 +13,7 @@ CREATE TABLE DOG (
     Breed VARCHAR(45) NULL,
     Gender CHAR NULL,
     Weight FLOAT NULL,
+    Neuter boolean null, 
     BCS VARCHAR(20) NULL,
     PRIMARY KEY (DogId), 
     FOREIGN KEY (UserId) REFERENCES USER (UserId)
@@ -74,9 +75,9 @@ CREATE TABLE  DISPENSER (
 Insert into USER values ('yoon8720', 'king01', 'dungdunge', 'yoon8720@nate.com', now());
 Insert into USER values ('kaya01', 'kwon0871', 'android369', 'kaya@gmail.com', now());
 
-Insert into DOG values ('dung2', 'yoon8720', 'shihtzu', 'M', 8.7, 'Normal');
-Insert into DOG values ('happy', 'yoon8720', 'maltese', 'M', 3.1, 'Obesity');
-Insert into DOG values ('bbobbi', 'kaya01', 'shihtzu', 'F', 2.7, 'Underweight');
+Insert into DOG values ('dung2', 'yoon8720', 'shihtzu', 'M', 8.7, true, 'Normal');
+Insert into DOG values ('happy', 'yoon8720', 'maltese', 'M', 3.1, true, 'Obesity');
+Insert into DOG values ('bbobbi', 'kaya01', 'shihtzu', 'F', 2.7, false,'Underweight');
 
 Insert into BCSIMAGE values(1, 'dung2', null);
 
@@ -86,8 +87,8 @@ Insert into DISPENSER values (now(), 'dung2', 20);
 Insert into SENSOR values ('2023-11-02 10:30:00', 'dung2', 1.98, -0.03, -2.67, -4.94, 105.35, -83.68, 52.1, 36.8, 59.1);
 Insert into SENSOR values (now(), 'dung2', 2.98, -0.08, 1.67, 2.94, -80.34, -43.65, 62.1, 36.8, 59.1);
 
-Insert into Activity values ('2023-11-02 10:30:00', 'dung2', 0);
-Insert into Activity values ('2023-11-02 10:31:00', 'dung2', 2);
+Insert into Activity values ('2023-11-02 10:30:00', 'dung2', 0, 1, 0, 1);
+Insert into Activity values ('2023-11-02 10:31:00', 'dung2', 0, 0, 1, 10);
 
 Insert into ActKcal values ('2023-11-02 10:31:00', 'dung2', 2, 150);
 Insert into ActKcal values (now(), 'happy', 1, 100);
