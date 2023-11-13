473e36112cffed7f084b41247b6bd22639dec4e3
ella00100 <ella00100@github.com>
2023-11-13 20:49:15 +0900

    feat : user dash page

--- app/src/index.js
+++ app/src/index.js
@@ -8,6 +8,7 @@ import signUpRouter from './routes/signUp';
 import signInAdminRouter from './routes/signInAdmin';
 import signInOwnerRouter from './routes/signInOwner';
 import ownerDashRouter from './routes/ownerDash';
+import userDashRouter from './routes/userDash';
 import sensorRouter from './routes/sensor';
 import regPetRouter from './routes/regPet';
 import uploadRouter from './routes/upload';
@@ -41,6 +42,8 @@ app.use('/ownerDash',ownerDashRouter);
 
 app.use('/regPet', regPetRouter);
 app.use('/upload', uploadRouter);
+app.use('/userDash', userDashRouter);
+
 app.use('/sensor', sensorRouter);
 
 app.listen(PORT, ()=> {
