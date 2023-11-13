f720656b774bc4638ad81ecfca9b9819cd7b14e8
kwon <12182193@inha.edu>
2023-11-13 16:17:11 +0900

    uploadfinal2

--- app/model/efficientnet.py
+++ app/model/efficientnet.py
@@ -17,38 +17,36 @@
 # def predict(model, images):
 #     model.eval()
 #     with torch.no_grad():
-#         # 평균 이미지 계산
-#         avg_image = torch.mean(images, dim=0, keepdim=True)
+#         predictions = []
+#         for image in images:
+#             output = model(image.unsqueeze(0))
+#             _, predicted = torch.max(output, 1)
+#             predictions.append(predicted.item())
+#         return predictions
 
-#         # 예측 수행
-#         output = model(avg_image)
-#         _, predicted = torch.max(output, 1)
-#         return predicted.item()
-
-# # 이미지 변환 정의
+# # Image transformation definition
 # transform = transforms.Compose([
 #     transforms.Resize((224, 224)),
 #     transforms.ToTensor(),
+#     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 # ])
 
-# # 모델 정의 및 로드
-# #model = EfficientNet.from_pretrained('efficientnet-b0.pth', num_classes=3)
+# # Model definition and loading
 # model = EfficientNet.from_name('efficientnet-b0', num_classes=3)
-# #model.load_state_dict(torch.load(r'app\model\model_epo5.pth'))
-# model = torch.load('app\model\model_epo5.pth', map_location=torch.device('cpu'))
+# model.load_state_dict(torch.load('model\\model_epo5.pth', map_location=torch.device('cpu')))
 # model.to(torch.device("cuda" if torch.cuda.is_available() else "cpu"))
 
-# # 인자로부터 이미지 경로 로드
-# image_paths = sys.argv[1:]  # 첫 번째 인자는 스크립트 이름이므로 제외
+# # Arguments for image paths
+# image_paths = sys.argv[1:]
 # if len(image_paths) != 13:
 #     raise ValueError("Exactly 13 images are required.")
 
-# # 이미지 로드 및 전처리
+# # Load and preprocess images
 # images = load_and_preprocess_images(image_paths, transform)
 
-# # 예측 수행
-# prediction = predict(model, images)
-# print("Prediction:", prediction)
+# # Perform prediction
+# predictions = predict(model, images)
+# print("Predictions:", predictions)
 
 import sys
 import torch
@@ -69,12 +67,16 @@ def load_and_preprocess_images(image_paths, transform):
 def predict(model, images):
     model.eval()
     with torch.no_grad():
-        predictions = []
-        for image in images:
+        # Instead of predicting for each image, we predict for all and average the outputs
+        outputs = torch.zeros([len(images), 3])  # Assuming 3 classes as in the original script
+        for i, image in enumerate(images):
             output = model(image.unsqueeze(0))
-            _, predicted = torch.max(output, 1)
-            predictions.append(predicted.item())
-        return predictions
+            outputs[i] = torch.nn.functional.softmax(output, dim=1)
+
+        # Averaging the predictions
+        avg_output = outputs.mean(dim=0)
+        _, predicted = torch.max(avg_output, 0)
+        return predicted.item()
 
 # Image transformation definition
 transform = transforms.Compose([
@@ -97,6 +99,10 @@ if len(image_paths) != 13:
 images = load_and_preprocess_images(image_paths, transform)
 
 # Perform prediction
-predictions = predict(model, images)
-print("Predictions:", predictions)
+prediction = predict(model, images)
 
+# Map the predicted index to the corresponding label
+class_labels = ["Ideal", "Low", "Overweight"]
+predicted_label = class_labels[prediction]
+
+print(predicted_label)
