73c9bd340201cfae0e22f6b85b64951dd069f0dd
kwon <12182193@inha.edu>
2023-11-13 20:52:33 +0900

    uploadfinal6

--- app/model/efficientnet.py
+++ app/model/efficientnet.py
@@ -30,7 +30,7 @@ def predict(model, images):
 
 # Image transformation definition
 transform = transforms.Compose([
-    transforms.Resize((224, 224), Image.ANTIALIAS),# Resizing the image
+    transforms.Resize((224, 224)),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
